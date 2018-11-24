0bb3b0e1e8c46d0ea8de3095782f3bf02e0e68b2
b5810450733 <tanakrit.pho@ku.th>
2018-11-24 17:29:08 +0700

    Initial commit

---
+++ src/main/java/Model/Subject.java
@@ -0,0 +1,17 @@
+package Model;
+
+public class Subject {
+    private String subCode;
+    private String subName;
+    private int creDit;
+    private int hardness;
+
+    public Subject(String subCode, String subName, int creDit, int hardness) {
+        this.subCode = subCode;
+        this.subName = subName;
+        this.creDit = creDit;
+        this.hardness = hardness;
+    }
+
+
+}
