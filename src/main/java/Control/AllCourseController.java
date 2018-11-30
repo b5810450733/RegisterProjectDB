673f4fed3309b9f42faad5e331e78d7a2882790b
b5810450733 <tanakrit.pho@ku.th>
2018-11-30 17:06:21 +0700

    แสดงหน้าของทุกวิช , ตกแต่ง , keypress ห้ามลง

--- src/main/java/Control/AllCourseController.java
+++ src/main/java/Control/AllCourseController.java
@@ -1,6 +1,29 @@
 package Control;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+
 public class AllCourseController {
+    @FXML
+    private TableColumn<?, ?> id;
+
+    @FXML
+    private TableColumn<?, ?> name;
+
+    @FXML
+    private TableColumn<?, ?> credit;
+
+    @FXML
+    private TableColumn<?, ?> year;
+
+    @FXML
+    private TableColumn<?, ?> base;
+
+    @FXML
+    private TableColumn<?, ?> contsub;
+
+    @FXML
+    private TableColumn<?, ?> diff;
 
 
 }
